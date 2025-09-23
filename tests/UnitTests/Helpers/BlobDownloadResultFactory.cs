4fdd25f3d27edde85bb26351bb90ad105b4fabd6
Pol de la hoz <p.delahoz@asts.es>
2025-09-23 15:42:43 +0200

    fix test

--- tests/UnitTests/Helpers/BlobDownloadResultFactory.cs
+++ tests/UnitTests/Helpers/BlobDownloadResultFactory.cs
@@ -1,26 +1,41 @@
-﻿using Azure.Storage.Blobs.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Azure;
+using Azure.Storage.Blobs.Models;
 
 namespace FlowStorageTests.UnitTests.Helpers
 {
     public static class BlobDownloadResultFactory
     {
-        public static BlobDownloadResult CreateFake(string content)
+        public static BlobDownloadResult CreateFake(string content, string contentType = "text/plain")
         {
-            var blobDownloadResult = (BlobDownloadResult)Activator.CreateInstance(
-                typeof(BlobDownloadResult),
-                BindingFlags.Instance | BindingFlags.NonPublic,
-                binder: null,
-                args: null,
-                culture: null)!;
+            BlobDownloadDetails details = BlobsModelFactory.BlobDownloadDetails(
+                lastModified: DateTimeOffset.UtcNow,
+                eTag: new ETag("0x8D5E48E5EC83E28"), // Un valor de ETag de ejemplo
+                contentLanguage: null,
+                contentDisposition: null,
+                cacheControl: null,
+                contentEncoding: "utf-8", // Puedes extraerlo del contentType
+                copyCompletedOn: DateTimeOffset.UtcNow,
+                copyStatusDescription: null,
+                copyId: null,
+                copyProgress: null,
+                copySource: null,
+                copyStatus: CopyStatus.Success,
+                contentHash: null,
+                isServerEncrypted: true,
+                encryptionKeySha256: null,
+                encryptionScope: null,
+                blobSequenceNumber: 0,
+                blobCommittedBlockCount: 1,
+                versionId: null,
+                createdOn: DateTimeOffset.UtcNow,
+                metadata: null,
+                contentType: contentType 
+            );
 
-            var contentProperty = typeof(BlobDownloadResult).GetProperty("Content", BindingFlags.Instance | BindingFlags.Public);
-            contentProperty!.SetValue(blobDownloadResult, BinaryData.FromString(content));
+            BlobDownloadResult blobDownloadResult = BlobsModelFactory.BlobDownloadResult(
+                details: details,
+                content: BinaryData.FromString(content)
+            );
 
             return blobDownloadResult;
         }
